2fe33a3569b073cb304b92b3dd7fa5018505b7d8
Mahmoud Taha <tmahmoud311@gmail.com>
2024-07-14 15:07:30 +0300

    Add Class Person

---
+++ LAP2/Preson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAP2
+{
+    internal class Preson
+    {
+        public int Name { get; set; }
+
+        public int Age { get; set; }
+
+        public  int SSN {  get; set; } 
+    }
+}
